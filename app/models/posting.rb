de84eb5aff22d070b84837673e5483d56edfad18
Riki Montgomery <mostriki820@gmail.com>
2017-12-20 15:05:44 -0800

    Create working table and make it a partial.

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -23,17 +23,16 @@ class Posting < ApplicationRecord
   # validates :discrimination, :presence => true
 
   def self.craigslist_scraper(city_url)
-    # nokogiri_object = Nokogiri::HTML(open(city_url).read)
-    # posting_link = nokogiri_object.xpath("//a[@class='result-title hdrlnk']/@href")
+    nokogiri_object = Nokogiri::HTML(open(city_url).read)
+    posting_link = nokogiri_object.xpath("//a[@class='result-title hdrlnk']/@href")
 
-    # posting_link.each do |link|
-      nokogiri_object2 = Nokogiri::HTML(open("https://portland.craigslist.org/clc/apa/d/single-level-updated-two/6431978804.html").read)
+    posting_link.each do |link|
+      nokogiri_object2 = Nokogiri::HTML(open(link).read)
       posting_title = nokogiri_object2.xpath("//span[@id='titletextonly']").text
       posting_specific_location = nokogiri_object2.xpath("//span[@class='postingtitletext']/small").text.gsub(/[()]/, '')
       posting_body = nokogiri_object2.xpath("//section[@id='postingbody']").text
-      posting_price = nokogiri_object2.xpath("//span[@class='price']").text.gsub!('$','').to_d.floor
+      posting_price = nokogiri_object2.xpath("//span[@class='price']").text.gsub!('$','').to_i
       posting_bedroom = nokogiri_object2.xpath("//span[@class='shared-line-bubble'][1]/b[1]").text.to_i
-      binding.pry
       posting_square_feet = nokogiri_object2.xpath("//span[@class='shared-line-bubble'][2]/b").text.to_i
       posting_address = nokogiri_object2.xpath("//div[@class='mapaddress']").text
       posting_post_id = nokogiri_object2.xpath("//p[@class='postinginfo'][1]").text.gsub(/\D/, '').to_i
@@ -42,9 +41,9 @@ class Posting < ApplicationRecord
       # posting_email_address = .xpath("//a[@class='mailapp']")
       # posting_phone_number = .xpath("//p[@class='reply-tel-number']")
 
-      Posting.create!(posting_link: nil,
+      Posting.create!(posting_link: link,
                       posting_title: posting_title,
-                      posting_body: posting_body,
+                      posting_body: "No HUD/Vouchers",
                       # remove decimal point or add 2
                       posting_price: posting_price,
                       posting_bedroom: posting_bedroom,
@@ -64,6 +63,6 @@ class Posting < ApplicationRecord
                       # complete: nil,
                       # discrimination: nil
                       )
-    # end
+    end
   end
 end
